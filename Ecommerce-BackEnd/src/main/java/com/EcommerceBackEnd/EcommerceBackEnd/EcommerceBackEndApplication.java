b7a9107e9dec1ef09fc923fb888db1ae3cec1e86
phuquy-git <nguyenngocphuquy98@gmail.com>
2021-11-04 14:56:07 +0700

    replacing project

---
+++ Ecommerce-BackEnd/src/main/java/com/EcommerceBackEnd/EcommerceBackEnd/EcommerceBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.EcommerceBackEnd.EcommerceBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackEndApplication.class, args);
+	}
+
+}
--- Ecommerce-BackEnd/src/test/java/com/EcommerceBackEnd/EcommerceBackEnd/EcommerceBackEndApplicationTests.java
+++ Ecommerce-BackEnd/src/test/java/com/EcommerceBackEnd/EcommerceBackEnd/EcommerceBackEndApplicationTests.java
@@ -1,10 +1,10 @@
-package com.backend.ecommerce.backend.ecommerce;
+package com.EcommerceBackEnd.EcommerceBackEnd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class EcommerceBackEndApplicationTests {
 
 	@Test
 	void contextLoads() {
